affe930719540a69f97a8bc1a5234e49367623f5
Ajayi Olamide <prodbyola@gmail.com>
2026-08-11 08:23:40 +0100

    Counter added.

---
+++ counter/app.js
@@ -0,0 +1,23 @@
+//set initial count
+let count = 0;
+
+
+//select values and button
+const value = document.querySelector('#value')
+const btns = document.querySelectorAll('.btn')
+console.log(btns);
+
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        const style = e.currentTarget.classList
+        if (style.contains( "decrease")){
+            count--;
+        }else if (style.contains("increase")){
+            count++;
+        }else {
+            count=0;
+        }
+        value.textContent=count;
+    });
+});
